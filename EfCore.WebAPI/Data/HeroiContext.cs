7ab2e39f3273f5cc5b9433df9616af432ca80f6f
Leandro Pinheiro Leite <leandro1.loudness@gmail.com>
2022-03-21 18:51:16 -0300

    decimo sexto commit

---
+++ EfCore.WebAPI/Data/HeroiContext.cs
@@ -0,0 +1,30 @@
+﻿using EfCore.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EfCore.WebAPI.Data
+{
+    public class HeroiContext: DbContext
+    {
+        public DbSet<Heroi> Herois { get; set; }
+        public DbSet<Batalha> Batalhas { get; set; }
+        public DbSet<Arma> Armas { get; set; }
+        public DbSet<HeroiBatalha> HeroisBatalhas { get; set; }
+        public DbSet<IdentidadeSecreta> IdentidadeSecretas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=HeroApp;Data Source=NAGILAPC");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<HeroiBatalha>(entity => {
+                entity.HasKey(e => new { e.BatalhaId, e.HeoiId });
+            });
+        }
+    }
+}
---
+++ EfCore.WebAPI/Models/HeroiBatalha.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EfCore.WebAPI.Models
+{
+    public class HeroiBatalha
+    {
+        public int HeoiId { get; set; }
+        public Heroi Heroi { get; set; }
+        public int BatalhaId { get; set; }
+        public Batalha Batalha { get; set; }
+    }
+}
